f3297518e94feda5d4be22f313f038416be7d3c4
johngaitho05 <johngaitho05@gmail.com>
2019-05-03 00:23:11 +0300

    added base template

--- jportfolio/settings.py
+++ jportfolio/settings.py
@@ -56,7 +56,10 @@ ROOT_URLCONF = 'jportfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR ,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates'),
+                 os.path.join(BASE_DIR,'jobs/templates',),
+                 os.path.join(BASE_DIR,'blog/templates',),
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

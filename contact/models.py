24a7bdd03598a9ff8461039d7de2ab58879ba83f
johngaitho05 <johngaitho05@gmail.com>
2019-05-03 20:18:03 +0300

    modified homepage and added contact app

---
+++ contact/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Message(models.Model):
+    name = models.CharField(max_length=250)
+    email = models.CharField(max_length=500)
+    message = models.TextField
+
+
+

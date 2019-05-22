38c1dd8843b47879d0fc6dfc378ebedf064cda20
johngaitho05 <johngaitho05@gmail.com>
2019-05-22 12:15:07 +0300

    bio edit

--- jportfolio/settings.py
+++ jportfolio/settings.py
@@ -58,7 +58,10 @@ ROOT_URLCONF = 'jportfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates'),
+                 os.path.join(BASE_DIR,'blog/templates'),
+                 os.path.join(BASE_DIR,'jobs/templates'),
+                 os.path.join(BASE_DIR,'contact/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
